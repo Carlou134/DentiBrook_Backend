e81767f1aa52f52e1a22bde8b3dfb3fadc220864
Victor Alejandro Arévalo Díaz <142472046+VictorArevalo0407@users.noreply.github.com>
2023-09-05 07:10:07 -0500

    cita

---
+++ src/main/java/pe/edu/upc/aaw/dentibrook_backend/dtos/CitaDTO.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.aaw.dentibrook_backend.dtos;
+
+public class CitaDTO {
+}
---
+++ src/main/java/pe/edu/upc/aaw/dentibrook_backend/entities/Cita.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.aaw.dentibrook_backend.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Cita")
+public class Cita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_Cita;
+    private int paciente_id;
+    private int odontologo_id;
+    private int tipopago_id;
+    private int duracionenhoras;
+    @OneToOne
+    @Column(name = "hora", length = 5)
+    private String hora;
+    @Column(name = "motivo", length = 100)
+    private String motivo;
+
+
+    @Column(name = "estado", length = 10)
+    private String estado;
+    @Column(name = "direccion_consultorio", length = 30)
+    private String direccion_consultorio;
+    @Column(name = "tipo_cita", length = 10)
+    private String tipo_cita;
+    @Column(name = "fecha", length = 10)
+    private LocalDate fecha;
+}
